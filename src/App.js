ca4c7e37129dfade9093ab23d299e65ce84574ae
G-iTech <G-iTech@hotmail.com>
2025-07-03 04:32:45 +0530

    Upload entire folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+// This is the main component of the Volunteer Yatra application
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Volunteer Yatra 🚀</h1>
+      <p>Your journey to meaningful travel begins here.</p>
+    </div>
+  );
+}
+
+export default App;
